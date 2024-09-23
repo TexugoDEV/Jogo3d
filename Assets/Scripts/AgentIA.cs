52f6ae03953cc73ee4e5990a19d04215884c193c
TexugoDEV <yurirafaelninja@gmail.com>
2024-09-23 19:20:47 -0300

    :construct: IA Test

---
+++ Assets/Scripts/AgentIA.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentIA : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<Transform> wayPoint;
+    NavMeshAgent navMeshAgent;
+    public int currentWaypointIndex = 0;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Walking();
+    }
+    private void Walking()
+    {
+        if (wayPoint.Count == 0)
+        {
+            return;
+        }
+        float distanceToWaypoint = Vector3.Distance(wayPoint[currentWaypointIndex
+            ].position, transform.position);
+
+        if (distanceToWaypoint <- 2)
+        {
+            currentWaypointIndex = (currentWaypointIndex + 1) % wayPoint.Count;
+        }
+
+        navMeshAgent.SetDestination(wayPoint[currentWaypointIndex].position);
+    }
+}
